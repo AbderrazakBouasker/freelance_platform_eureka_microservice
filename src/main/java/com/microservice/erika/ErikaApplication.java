c8f254b4f38bfeb6ebd0775fd7820460c4955f78
abderrazak bouasker <abderrazakbouasker@gmail.com>
2024-10-09 20:20:04 +0100

    init

---
+++ src/main/java/com/microservice/erika/ErikaApplication.java
@@ -0,0 +1,13 @@
+package com.microservice.erika;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErikaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErikaApplication.class, args);
+	}
+
+}
